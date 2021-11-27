af25b2fd5c2f332d73312664252f3819100dc54d
vpanyushenko <me@panyushenko.com>
2021-11-26 22:20:11 -0500

    Add id's to sidebar sectiojs

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import "$lib/css/app.css";
+  import "../code.css";
 
   import Sidebar from "$lib/components/sidebar/Sidebar.svelte";
   import SidebarPageWrapper from "$lib/layouts/SidebarPageWrapper.svelte";
@@ -12,61 +13,73 @@
     sections: [
       {
         title: "Welcome",
+        id: "welcome",
         href: "/",
         section: "Getting Started",
       },
       {
         title: "Installation",
+        id: "installation",
         href: "#installation",
         section: "Getting Started",
       },
       {
         title: "Usage",
+        id: "usage",
         href: "#usage",
         section: "Getting Started",
       },
       {
         title: "Colors",
+        id: "colors",
         href: "#colors",
         section: "Getting Started",
       },
       {
-        title: "Page Layouts",
-        href: "#page-layouts",
+        title: "Page Wrappers",
+        id: "page-wrappers",
+        href: "#page-wrappers",
         section: "Layouts",
       },
       {
         title: "Page Rows",
+        id: "page-rows",
         href: "#page-rows",
         section: "Layouts",
       },
       {
         title: "Page Columns",
-        href: "#page-columnss",
+        id: "page-columns",
+        href: "#page-columns",
         section: "Layouts",
       },
       {
         title: "Page Grids",
+        id: "page-grids",
         href: "#page-grids",
         section: "Layouts",
       },
       {
         title: "Page Center",
+        id: "page-center",
         href: "#page-center",
         section: "Layouts",
       },
       {
         title: "Buttons",
+        id: "buttons",
         href: "#buttons",
         section: "Components",
       },
       {
         title: "Cards",
+        id: "cards",
         href: "#cards",
         section: "Components",
       },
       {
         title: "Checkboxes",
+        id: "checkboxes",
         href: "#checkboxes",
         section: "Components",
       },
