4a081278dfe486fa2e2cbf92e156cc210ab96fa7
vpanyushenko <vpanyushenko@outlook.com>
2021-11-30 21:13:32 -0800

    Add footer prop

--- src/lib/stores/modal.store.js
+++ src/lib/stores/modal.store.js
@@ -1,3 +1,3 @@
 import { writable } from "svelte/store";
 
-export const modalStore = writable({ title: "", active: false, form: [], message: "" });
+export const modalStore = writable({ title: "", active: false, form: [], message: "", footer: [] });
