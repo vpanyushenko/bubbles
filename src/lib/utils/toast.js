bece34c6f7b1bdef4d76177d9802f2a6a4a55cb8
vpanyushenko <vpanyushenko@outlook.com>
2021-12-01 22:08:53 -0800

    Update type to color

--- src/lib/utils/toast.js
+++ src/lib/utils/toast.js
@@ -1,27 +1,29 @@
 import { v4 as uuid } from "@lukeed/uuid";
 import { toastStore } from "$lib/stores/toast.store";
+import { now } from "svelte/internal";
 
 /**
  * Will show a toast notification
  * @param {String} [message="An error occured"] - The message that will be shown in the toast
- * @param {String} [type="error"] - The type of toast this is. Default is error, otherwise use success
+ * @param {String} [color="error"] - The color of toast this is. Default is error, otherwise use success
  */
-const showToast = (message = "An error occured", type = "error") => {
+const showToast = (message = "An error occured", color = "error") => {
   const id = uuid();
 
   toastStore.update((data) => {
     const toast = {
       message: message,
-      type: type,
+      color: color,
       active: true,
       id: id,
+      timestamp: Date.now(),
     };
 
     if (data === undefined || data === null) {
       data = [];
     }
     data.unshift(toast);
-    console.log(data);
+
     return data;
   });
 };
