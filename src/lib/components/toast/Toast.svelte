808fc0cd010ce6675d566de7f77ae176b344c505
vpanyushenko <vpanyushenko@outlook.com>
2021-12-01 22:08:42 -0800

    Add colors and fix remove toasts bug where some toasts wouldn't go away

--- src/lib/components/toast/Toast.svelte
+++ src/lib/components/toast/Toast.svelte
@@ -1,10 +1,10 @@
 <script>
   import { fly, fade } from "svelte/transition";
   import close from "./close.svg";
-  import { toastStore } from "$lib/utils/toast";
+  import { configStore, toastStore } from "$lib/stores/stores";
 
   export let active = false;
-  export let type = "success";
+  export let color = "success";
   export let message;
   export let id;
 
@@ -20,17 +20,31 @@
 
   setTimeout(() => {
     toastStore.update((toasts) => {
-      toasts.splice(index, 1);
-      return toasts;
+      const filteredToasts = toasts
+        .map((toast) => {
+          if (Date.now() - toast.timestamp + 50 < $configStore.toast_delay) {
+            return toast;
+          } else {
+            return null;
+          }
+        })
+        .filter(Boolean);
+      return filteredToasts;
     });
-  }, 5500);
+  }, $configStore.toast_delay);
 </script>
 
 {#if active}
   <div
     class="toast"
-    class:success={type === "success"}
-    class:error={type === "error"}
+    class:primary={color === "primary"}
+    class:secondary={color === "secondary"}
+    class:error={color === "error"}
+    class:warning={color === "warning"}
+    class:success={color === "success"}
+    class:info={color === "info"}
+    class:gray={color === "gray"}
+    class:dark={color === "dark"}
     {id}
     style="top: {top}"
     in:fly={{ duration: 300, y: -100 }}
@@ -60,14 +74,6 @@
     border-radius: 8px;
   }
 
-  .toast.success {
-    background-color: var(--success);
-  }
-
-  .toast.error {
-    background-color: var(--error);
-  }
-
   img {
     opacity: 0.4;
     padding: 0 5px;
@@ -91,6 +97,82 @@
     left: 0;
     right: 0;
     max-width: fit-content;
+  }
+
+  .primary {
+    background: var(--primary);
+    color: var(--white);
+  }
+
+  .primary:hover {
+    background: var(--primary-dark);
+  }
+
+  .secondary {
+    background: var(--secondary);
+    color: var(--white);
+  }
+
+  .secondary:hover {
+    background: var(--secondary-dark);
+  }
+
+  .error {
+    background: var(--error);
+    color: var(--white);
+  }
+
+  .error:hover {
+    background: var(--error-dark);
+  }
+
+  .warning {
+    background: var(--warning);
+    color: var(--white);
+  }
+
+  .warning:hover {
+    background: var(--warning-dark);
+  }
+
+  .success {
+    background: var(--success);
+    color: var(--white);
+  }
+
+  .success:hover {
+    background: var(--success-dark);
+  }
+
+  .success-light {
+    background: var(--success-lightest);
+    color: var(--success-darkest);
+  }
+
+  .info {
+    background: var(--info);
+    color: var(--white);
+  }
+
+  .info:hover {
+    background: var(--info-dark);
+  }
+
+  .dark {
+    background: var(--dark);
+    color: var(--white);
+  }
+
+  .dark:hover {
+    background: var(--dark-light);
+  }
+  .gray {
+    background: var(--gray);
+    color: var(--gray-lightest);
+  }
+
+  .gray:hover {
+    background: var(--gray-dark);
   }
 
   @media only screen and (max-width: 500px) {
