6971b97793e7f76c7c11992e3b518572dacb16ae
vpanyushenko <vpanyushenko@outlook.com>
2021-12-01 19:21:47 -0800

    Add option to fetch new data and show loading animations

--- src/lib/utils/url.js
+++ src/lib/utils/url.js
@@ -1,50 +1,33 @@
-// function pagination(action) {
-//   event.preventDefault();
-//   button.disabled = true;
-
-//   const urlParams = new URLSearchParams(window.location.search);
-//   // const limit = urlParams.get("limit");
-//   const page = urlParams.get("page");
-
-//   if (action === "next") {
-//     button.querySelectorAll("span")[1].classList.remove("hidden");
-//     button.querySelector("svg").classList.add("hidden");
-//     if (!page) {
-//       addQueryParam("page", 2);
-//     } else {
-//       addQueryParam("page", parseInt(page) + 1);
-//     }
-//   }
-
-//   if (action === "prev") {
-//     button.querySelectorAll("span")[0].classList.remove("hidden");
-//     button.querySelector("svg").classList.add("hidden");
-//     if (!page) {
-//       addQueryParam("page", 1);
-//     } else {
-//       let newPageNumber = parseInt(page) - 1 < 1 ? 1 : parseInt(page) - 1;
-//       addQueryParam("page", newPageNumber);
-//     }
-//   }
-
-//   if (action === "all") {
-//     button.querySelectorAll("span")[0].classList.remove("hidden");
-//     if (button.querySelector("svg")) {
-//       button.querySelector("svg").classList.add("hidden");
-//     }
-//     addQueryParam("page", "all");
-//   }
-// }
+import { browser } from "$app/env";
+import { goto } from "$app/navigation";
+import { showLoading, hideLoading } from "$lib/utils/loading";
 
 /**
  * Adds a url query param based on a key value pair
  * @param {String} key - The key for the url param you want to add
  * @param {String} value - The value for the key
+ * @param {Object} options - Additional options
+ * @param {Boolean} [options.goto=true] - Will rerun the svelte load function after the query param is added
+ * @param {String} [options.show_loading] - The ID of an element you want to shoe the animation for. The loading state will automatically stop when the load function completes it's promise.
  */
-const addQueryParam = (key, value) => {
-  const queryParams = new URLSearchParams(window.location.search);
-  queryParams.set(key, value);
-  history.replaceState(null, null, "?" + queryParams.toString());
+const addQueryParam = (key, value, options = { goto: true, show_loading: "" }) => {
+  if (browser) {
+    const queryParams = new URLSearchParams(window.location.search);
+    queryParams.set(key, value);
+    history.replaceState(null, null, "?" + queryParams.toString());
+
+    if (options?.goto || options?.show_loading) {
+      if (options?.show_loading) {
+        showLoading(options.show_loading);
+      }
+
+      goto(window.location.href, { keepfocus: true, replaceState: true, noscroll: true }).then(() => {
+        if (options.show_loading) {
+          hideLoading(options.show_loading);
+        }
+      });
+    }
+  }
 };
 
 export { addQueryParam };
