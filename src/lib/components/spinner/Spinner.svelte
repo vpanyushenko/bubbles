bb89e965db3b1aa7081aebe5c31d7ecf5603286b
vpanyushenko <vpanyushenko@outlook.com>
2021-12-06 13:00:47 -0800

    Init commit

---
+++ src/lib/components/spinner/Spinner.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let size = 1.25;
+  export let color = "white";
+
+  let style;
+
+  if (color === "white") {
+    style = `border-color: var(--gray-light); border-top-color: var(--gray-lightest);`;
+  } else if (color) {
+    style = `border-color: var(--${color}); border-top-color: var(--${color}-lightest);`;
+  }
+</script>
+
+<span class="spinner" style="width: {size}rem; height: {size}rem; {style}" />
+
+<style>
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .spinner {
+    content: "";
+    box-sizing: border-box;
+    width: 1.25rem;
+    height: 1.25rem;
+    border-radius: 50%;
+    border: 2px solid #ccc;
+    border-top-color: #fff;
+    animation: spinner 0.6s linear infinite;
+  }
+</style>
