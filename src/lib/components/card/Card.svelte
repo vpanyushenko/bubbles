9d2ee54a72f9989d73f40505066dc516882f84ee
vpanyushenko <vpanyushenko@outlook.com>
2021-12-20 15:40:51 -0800

    Added concept for transitions but commented out

--- src/lib/components/card/Card.svelte
+++ src/lib/components/card/Card.svelte
@@ -1,75 +1,10 @@
-<!--
-  @component
-
-  ## Cards
-
-  Cards are the focus of Bubbles and they will normally be the sections of your page. Cards will fit into whatever `Column` or `Grid` layouts you choose.About
-
-  ---
-  #### Anatomy
-  Cards have three sections, `header`, `body`, and `footer`
-  
-  - The `header` is normally used for the `title` + `action` items or `filters`, which are select elements. You'll use the filters most often when the `body` of your card has a table, as the filter should modify the page url trigger the svelte load function to run again
-  - The `body` is where you add your content. This can be a `Table`, `Form`, or any other element(s) you choose. It's a slot and takes no arguments
-  - The `footer` is the bottom of the card and off by default. Most uses for the card footer are for pagination of a table or for CTA buttons (though in a form it's best to include the CTA buttons in the form instead of the card footer)
-
-
-  #### Example
-
-  ```js
-  
-  //The card header is optional. If can include a title, subtitle, + button combo or filters. 
-
-  const headerConfig = {
-    title: "Card Title" //This string will appear at the top of your card
-    caption: "Card Subtitle" //If you need a subtitle,
-    actions: [
-        {
-          icon: "add", //prebundled icon or pass in your own svg
-          onclick: showModel() //add a function
-        },
-        {
-          icon: "/images/home.svg", //added a custom svg
-          href: "/", //href to take me to another page
-        },
-      ],
-    border: false //turn off the border, it's my by default
-    }
-
-    //The filter config is just a select element with an onselect function that should modify a url querty param which will rerun the svelte kit laod funciton
-    //Bubbles has a setQueryParam function which you can import from "bubbles/utils/url"
-
-    //TODO: The fitlers are unfinished
-  const headerFilterConfig = {
-    filters: [
-      {
-        label: "Filter by name",
-      }
-    ]
-  }
-
-  
-
-  //The footer
-  const footerConfig = {
-    align: "start" || "center" || "end" //justifies the footer
-    //if you have pagination for the template, you can add in the pagination prop
-    pagination: {
-        //TODO:
-    } 
-  }
-
-
-
-
-  ```
-
-  ---
--->
 <script>
   import { pageStore } from "$lib/stores/page.store";
   import CardHeader from "./CardHeader.svelte";
   import CardFooter from "./CardFooter.svelte";
+
+  // import { scale, fade, fly } from "svelte/transition";
+  // import { quintOut } from "svelte/easing";
 
   export let header = {};
   export let footer = {};
@@ -90,6 +25,9 @@
 
   $: innerWidth = 0;
   $pageStore.is_mobile = innerWidth < 767 ? true : false;
+
+  // in:fly={{ y: -50, duration: 250, delay: 300 }}
+  // out:fly={{ y: -50, duration: 250 }}
 </script>
 
 <svelte:window bind:innerWidth />
