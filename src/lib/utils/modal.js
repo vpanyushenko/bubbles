7ea87bb0470103038485ef34711baabe937ba9ca
vpanyushenko <me@panyushenko.com>
2022-01-24 18:10:33 -0800

    Set dynamic height

--- src/lib/utils/modal.js
+++ src/lib/utils/modal.js
@@ -9,12 +9,13 @@ import { modalStore } from "$lib/stores/modal.store";
  * @param {Array} options.form - An array of form inputs
  * @param {Array} options.footer - An array of buttons for the modal footer. Usually cancel and confirm combos.
  */
-const showModal = (title, options = { message: "", form: [], footer: [], img: null }) => {
+const showModal = (title, options = { message: "", form: [], footer: [], img: null, height: null }) => {
   const _options = {
     message: options.message,
     form: options.form ? options.form : [],
     footer: options.footer ? options.footer : [],
     img: options?.img || null,
+    height: options.height || null,
   };
 
   modalStore.update((data) => {
@@ -24,6 +25,7 @@ const showModal = (title, options = { message: "", form: [], footer: [], img: nu
     data.message = _options.message;
     data.footer = _options.footer;
     data.img = _options.img;
+    data.height = _options.height;
     return data;
   });
 };
