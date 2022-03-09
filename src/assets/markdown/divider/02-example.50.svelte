56c6c1cf014342fb8d3a16239c721330d9ad1b94
vpanyushenko <me@panyushenko.com>
2022-03-08 23:30:50 -0800

    Add divider

---
+++ src/assets/markdown/divider/02-example.50.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Card from "$lib/components/card/Card.svelte";
+  import CardHeader from "$lib/components/card/CardHeader.svelte";
+  import Divider from "$lib/components/divider/Divider.svelte";
+</script>
+
+<Card height100={true}>
+  <CardHeader title="Demo" border={false} />
+  <div class="d-flex">
+    <Divider label="TEXT IN THE MIDDLE" />
+  </div>
+</Card>
