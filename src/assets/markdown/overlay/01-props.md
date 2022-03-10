8cc51934f3f10b2ca1c54574c214f42932b537d1
vpanyushenko <me@panyushenko.com>
2022-03-09 21:00:49 -0800

    Add overlay

---
+++ src/assets/markdown/overlay/01-props.md
@@ -0,0 +1,30 @@
+# Overlay
+
+The overlay puts a page overtop of the whole page. It's useful if you want to temporarily block the current page content without navigating to a new page.
+
+For example, the `Error` and `Modal` components use overlays to show content without navigating to a new page.
+
+You'll want to include functionality to hide an show the `Overlay`, otherwise it will block all content on the screen.
+
+<br>
+
+---
+
+<br>
+
+## Props
+
+<br>
+
+**solid** `boolean` <code class="blue">false</code><br>
+Normally the overlay is a transparent black background, but you can force it to be a solid background by passing in true to this field.
+
+---
+
+**transition_duration** `number` <code class="blue">0</code> <br>
+Add the time in milliseconds that you want the overlay to fade in
+
+---
+
+**onclick** `function`<br>
+An function that will be fired when the overlay is clicked.
---
+++ src/assets/markdown/overlay/02-example.50.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Card from "$lib/components/card/Card.svelte";
+  import CardHeader from "$lib/components/card/CardHeader.svelte";
+
+  import Overlay from "$lib/components/overlay/Overlay.svelte";
+  import Spinner from "$lib/components/spinner/Spinner.svelte";
+  import Button from "$lib/components/button/Button.svelte";
+  import Center from "$lib/layouts/Center.svelte";
+
+  let overlayToggle = false;
+</script>
+
+<Card height100={true}>
+  <CardHeader title="Overlay Demo" border={false} />
+
+  {#if overlayToggle}
+    <Overlay solid={true} transition_duration={500} onclick={() => (overlayToggle = false)}>
+      <Center>
+        <Spinner size={5} color="primary" />
+        <p>Loading Your Data (Click anywhere to close the overlay)</p>
+      </Center>
+    </Overlay>
+  {/if}
+
+  <Button
+    label="Show Overlay"
+    wide={true}
+    mb={true}
+    onclick={() => {
+      overlayToggle = true;
+    }}
+  />
+</Card>
