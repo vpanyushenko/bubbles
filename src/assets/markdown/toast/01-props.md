6cc366c149b9561661ee010ff03fbe84597b1f12
vpanyushenko <me@panyushenko.com>
2022-03-10 00:39:06 -0800

    Add toast

---
+++ src/assets/markdown/toast/01-props.md
@@ -0,0 +1,5 @@
+# ToastContainer
+
+The `ToastContainer` makes it easy to include toasts in your app. Include the container in you `__layout.svelte` file, and then use the utility function `showToast` to add toasts to the page.
+
+The container has no properties and will already show toasts at the top right of the screen.
---
+++ src/assets/markdown/toast/03-example.50.svelte
@@ -0,0 +1,79 @@
+<script>
+  import Card from "$lib/components/card/Card.svelte";
+  import CardHeader from "$lib/components/card/CardHeader.svelte";
+
+  import Form from "$lib/components/form/Form.svelte";
+  import { validateInputs, getFormData } from "$lib/utils/form";
+  import { showToast } from "$lib/utils/toast";
+
+  const toastExampleFormInputs = [
+    {
+      type: "radio",
+      id: "toast_example_color",
+      value: "success",
+      label: "Select a toast color",
+      options: [
+        {
+          label: "primary",
+          value: "primary",
+        },
+        {
+          label: "secondary",
+          value: "secondary",
+        },
+        {
+          label: "error",
+          value: "error",
+        },
+        {
+          label: "warning",
+          value: "warning",
+        },
+        {
+          label: "success",
+          value: "success",
+        },
+        {
+          label: "info",
+          value: "info",
+        },
+        {
+          label: "gray",
+          value: "gray",
+        },
+        {
+          label: "dark",
+          value: "dark",
+        },
+      ],
+    },
+    {
+      type: "text",
+      id: "toast_example_message",
+      label: "Toast Message",
+      value: "Your settings were successfully updated!",
+      error: "Add a message that is at least 3 characters",
+      validation: "string|required|min:3",
+      vob: true,
+    },
+    {
+      type: "submit",
+      label: "Show Toast",
+      onsubmit: async () => {
+        try {
+          await validateInputs(toastExampleFormInputs);
+          const data = await getFormData(toastExampleFormInputs);
+          showToast(data.toast_example_message, data.toast_example_color);
+        } catch (error) {
+          showToast(error.message);
+        }
+      },
+    },
+  ];
+</script>
+
+<Card height100={true}>
+  <CardHeader title="Toast Example" border={false} />
+
+  <Form inputs={toastExampleFormInputs} />
+</Card>
