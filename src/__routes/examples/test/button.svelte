a14cef0f3f0728fb34766dbd8f121a7f42701351
vpanyushenko <vpanyushenko@outlook.com>
2022-03-27 03:42:04 -0700

    Delete all files

--- src/__routes/examples/test/button.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Button from "$lib/components/button/Button.svelte";
-  import { showLoading } from "$lib/utils/loading";
-
-  const id = "123";
-  //showLoading(id);
-</script>
-
-<Button label="Testing" href="/examples" {id} />
