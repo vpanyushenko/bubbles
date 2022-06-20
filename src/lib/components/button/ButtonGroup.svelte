20ac64ea4e95da986d7fbe05a79f2c53feaafb4d
vpanyushenko <vpanyushenko@outlook.com>
2022-06-19 21:39:27 -0700

    init commit

---
+++ src/lib/components/button/ButtonGroup.svelte
@@ -0,0 +1,40 @@
+<script>
+  import Button from "./Button.svelte";
+
+  export let mt = false;
+  export let mb = false;
+  export let buttons = [];
+</script>
+
+<div class="button__group" class:mb class:mt>
+  {#if Array.isArray(buttons) && buttons.length > 0}
+    {#each buttons as button}
+      <Button {...button} />
+    {/each}
+  {:else}
+    <slot />
+  {/if}
+</div>
+
+<style>
+  .button__group {
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+
+  :global(.button__group > button + .icon__btn),
+  :global(.button__group > .icon__btn + button),
+  :global(.button__group > .icon__btn + .icon__btn),
+  :global(.button__group > button + button) {
+    margin-left: 1rem;
+  }
+
+  .mb {
+    margin-bottom: 1rem;
+  }
+
+  .mt {
+    margin-top: 1rem;
+  }
+</style>
