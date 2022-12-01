38d27d34f4007d6e44b2b8e7d0097982a9596878
vpanyushenko <vpanyushenko@outlook.com>
2022-12-01 12:02:59 -0800

    Testing

---
+++ src/wip-routes/+layout.svelte
@@ -0,0 +1,82 @@
+<script>
+  import "$lib/css/app.css";
+  import "$docs/css/oceanic.css";
+  import "$docs/css/markdown.css";
+
+  import icon_arrow from "$docs/icons/arrow.svg";
+
+  import Sidebar from "$lib/components/sidebar/Sidebar.svelte";
+  import SidebarPageWrapper from "$lib/layouts/SidebarPageWrapper.svelte";
+
+  import ToastContainer from "$lib/components/toast/ToastContainer.svelte";
+  import IconButton from "$lib/components/button/IconButton.svelte";
+  import sections from "$docs/utils/sidebar-sections";
+  import store from "$docs/utils/store";
+  import ModalContainer from "$lib/components/modal/ModalContainer.svelte";
+
+  import logo from "$docs/icons/logo.svg";
+  import logo_dark_mode from "$docs/icons/logo-dark-mode.svg";
+
+  // $configStore.padding = "compact";
+  // $configStore.radius = "blocky";
+
+  $: sidebarConfig = {
+    //logo: "/logo.svg",
+    //logo_dark: "/logo-dark-mode.svg", //IDK why this doesn't work
+    logo: logo,
+    logo_dark: logo_dark_mode,
+    flat: $store.flat,
+    padding: $store.sidebar_padding,
+    sections: sections.map((section) => {
+      // section.icon = icon;
+      // delete section.group;
+      return section;
+    }),
+  };
+
+  // $: if ($store.sidebar_index) {
+  //   setTimeout(() => {
+  //     $pageStore.sidebar.active_item = sidebarConfig.sections[$store.sidebar_index].id;
+  //   }, 750);
+  // }
+
+  let y = 0;
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<ToastContainer />
+
+<Sidebar {...sidebarConfig} />
+
+<SidebarPageWrapper>
+  <slot />
+</SidebarPageWrapper>
+
+<ModalContainer />
+
+{#if y > 1000}
+  <div>
+    <IconButton icon={icon_arrow} color="gray-lighter" onclick={() => (y = 0)} />
+  </div>
+{/if}
+
+<style>
+  :global(p) {
+    margin-bottom: 1rem;
+    color: var(--gray-darker);
+  }
+
+  :global(.mt-2) {
+    margin-top: 2rem;
+  }
+
+  div {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    z-index: 10;
+  }
+</style>
---
+++ src/wip-routes/[component]/+page.server.js
@@ -0,0 +1,59 @@
+import fs from "fs";
+import path from "path";
+// import { read_file } from "$lib/docs/server";
+import { error } from "@sveltejs/kit";
+
+const base = "../../assets";
+
+import breadcumbs from "$lib/utils/breadcrumbs";
+console.log(fs.existsSync("../../lib/utils/breadcrumbs"));
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+  console.log(breadcumbs);
+  /* @vite-ignore */
+  const markdown = await import(`$docs/markdown/${params.component}`);
+
+  console.log(markdown);
+  console.log(base);
+  console.log(base);
+  console.log(base);
+  const test = fs.existsSync("../../lib");
+  console.log(test);
+  console.log(test);
+  console.log(test);
+  console.log(fs.existsSync("./"));
+  console.log(fs.existsSync("../"));
+  console.log(fs.existsSync("../../"));
+  console.log(fs.existsSync("../../lib"));
+  console.log(fs.existsSync("$lib"));
+  console.log(fs.existsSync("$lib/utils"));
+  //   console.log(fs.existsSync("$docs"));
+  //   console.log(fs.existsSync(base));
+
+  const dir = `${base}/${params.component}`;
+  console.log(fs.statSync(base));
+  console.log("base");
+  console.log("base");
+  console.log("base");
+  console.log("base");
+  console.log(dir);
+  console.log(__dirname);
+
+  console.log(fs.readdirSync(path.join(__dirname, dir)));
+
+  //   for (const subdir of fs.readdirSync(base)) {
+  //   	if (!fs.statSync(`${base}/${subdir}`).isDirectory()) continue;
+
+  for (const file of fs.readdirSync(dir)) {
+    console.log(file);
+    // if (file.slice(3, -3) === params.slug) {
+    // 	return {
+    // 		page: await read_file(`docs/${subdir}/${file}`)
+    // 	};
+    // }
+  }
+  //   }
+
+  // throw error(404);
+}
