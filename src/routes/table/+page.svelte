d621371eadc514dc14b2d2f81dba75b82bc58c95
vpanyushenko <vpanyushenko@outlook.com>
2022-12-28 18:18:39 -0800

    Reactive vars

--- src/routes/table/+page.svelte
+++ src/routes/table/+page.svelte
@@ -1,15 +1,15 @@
 <script>
   import Rows from "$docs/components/Rows.svelte";
-  import Header from "$lib/components/header/Header.svelte";
   import { formatPosts } from "$docs/utils/posts";
   import store from "$docs/utils/store";
+
   const all_posts = import.meta.glob(`../../docs/markdown/table/**/*`, { eager: true });
   const rows = formatPosts(all_posts);
 
   export let data;
 
-  $store.pokemon = data.pokemon;
-  $store.pagination = data.pagination;
+  $: $store.pokemon = data.pokemon;
+  $: $store.pagination = data.pagination;
 </script>
 
 <Rows {rows} />
