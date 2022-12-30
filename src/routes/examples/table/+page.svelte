9d62fb34a7d7c2b7fec09cf19b9bf82d6f725e1f
vpanyushenko <vpanyushenko@outlook.com>
2022-12-30 15:25:06 -0800

    testing

---
+++ src/routes/examples/table/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Row, Card, Column100, Table, TableRow, TableCell } from "$lib/index";
+</script>
+
+<Row>
+  <Column100>
+    <Card>
+      <Table>
+        <TableRow href="/table">
+          <TableCell text="Test" bold={true} />
+          <TableCell text="Hello World" caption="caption" />
+          <TableCell button />
+        </TableRow>
+
+        <TableRow>
+          <TableCell text="Test" bold={true} href="/table" />
+          <TableCell caption="Only Caption" />
+          <TableCell button />
+        </TableRow>
+
+        <TableRow>
+          <TableCell text="Test" bold={true} />
+          <TableCell caption="Only Caption" />
+          <TableCell button={{ href: "/table" }} />
+        </TableRow>
+      </Table>
+    </Card>
+  </Column100>
+</Row>
